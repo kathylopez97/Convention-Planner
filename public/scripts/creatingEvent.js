2aa12ba503537135d1ba0ac7572a4e830ba2fdfd
Daniel Jutila <daniel.jutila@gmail.com>
2024-02-14 21:35:39 -0600

    working on creating events

--- public/scripts/creatingEvent.js
+++ public/scripts/creatingEvent.js
@@ -1,6 +1,24 @@
-let eventName = document.getElementById('eventName');
-let startDate = document.getElementById('startDate');
-let startTime = document.getElementById('startTime');
-let price = document.getElementById('price');
-let description = document.getElementById('description');
+const eventForm = async (event) => {
+    event.preventDefault();
+    let eventName = document.getElementById('eventName');
+    let eventImage = document.getElementById('eventImage')
+    let startDate = document.getElementById('startDate');
+    let startTime = document.getElementById('startTime');
+    let price = document.getElementById('price');
+    let description = document.getElementByclas('description');
 
+    
+    const response = await fetch('/api/events', {
+        method: 'POST',
+        body: JSON.stringify({ eventName, startDate, startTime, price, description }),
+        headers: {'Content-Type': 'application/json'},
+    });
+    if(response.ok){
+        alert('a-okay');
+    } else {
+        alert('not a-okay');
+    }
+
+    
+};
+document.querySelector('.create-event').addEventListener('submit', eventForm);
