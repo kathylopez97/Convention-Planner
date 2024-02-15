b96c061a8703c783583acad4e67c740e0adbd1a2
Daniel Jutila <daniel.jutila@gmail.com>
2024-02-14 19:30:09 -0600

    added static html pages and rendered them in server.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-
+// router.use('/events', homeRoutes);
 module.exports = router;
