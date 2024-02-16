b2e87b68ee80a6c707fed9997fd52544ba2f49ba
Daniel Jutila <daniel.jutila@gmail.com>
2024-02-15 21:27:10 -0600

    Changed location of login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {Event, User} = require('../models');
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
