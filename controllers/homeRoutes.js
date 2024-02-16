2173e2ecb643541dee9d19ab2811ef2767841441
Daniel Jutila <daniel.jutila@gmail.com>
2024-02-15 20:31:18 -0600

    added homeroutes but commented out the logged in session

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,8 +6,25 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/login', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
   res.render('login');
 });
+
+router.get('/createevent', async (req, res) => {
+  // if(!req.session.loggedIn){
+  //   res.redirect('login');
+  //   return;
+  // }
+  res.render('createevent');
+});
+
+router.get('/dashboard', async (req, res) => {
+  // if(!req.session.loggedIn){
+  //   res.redirect('login')
+  //   return;
+  // }
+  res.render('dashboard');
+});
+
+
 
 module.exports = router;
