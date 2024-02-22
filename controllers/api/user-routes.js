1bfa021cdd7b7371f109d0905304276360c4236e
HunterBell512 <bellhunter512@gmail.com>
2024-02-22 01:44:35 -0500

    completed image uploading to the database

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,71 +1,75 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.post('/', async (req, res) => {
-    try {
-        const dbUserData = await User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            isCreator: req.body.isCreator
-        });
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      isCreator: req.body.isCreator,
+    });
 
-        req.session.save(() => {
-            req.session.loggedIn = true;
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.username = dbUserData.username;
+      req.session.userID = dbUserData.id;
 
-            res.status(200).json(dbUserData);
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const dbUserData = await User.findOne({
-            where: {
-                email: req.body.email,
-            }
-        });
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
 
-        if (!dbUserData) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password! Please try again.' })
-            return;
-        }
-
-        const correctPassword = dbUserData.checkPassword(req.body.password);
-
-        if (!correctPassword) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password! Please try again.' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-
-            res
-                .status(200)
-                .json({ user: dbUserData, message: 'Successfully logged in!' });
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password! Please try again." });
+      return;
     }
+
+    const correctPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!correctPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password! Please try again." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.username = dbUserData.username;
+      req.session.userID = dbUserData.id;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "Successfully logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const path = require('path');
-const handlebars = require('handlebars');
-const multer = require('multer');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const path = require("path");
+const handlebars = require("handlebars");
+const multer = require("multer");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
   cookie: {
     // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
@@ -23,7 +23,7 @@ const sess = {
     // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
     secure: false,
     // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
-    sameSite: 'strict',
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
@@ -36,47 +36,21 @@ const sess = {
 app.use(session(sess));
 
 const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Needs to be in order. Middleware runs first
-app.use(routes);
-
-// Potentiialy move
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/images/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, req.params.creatorID + '-eventImage' + path.extname(file.originalname));
-  }
-})
-const upload = multer({ storage: storage });
-
-app.post('/api/events/:creatorID', upload.single('eventImage'), async (req, res) => {
-  try {
-    const eventData = {
-      creatorID: req.params.creatorID,
-    };
-    const savedEvent = await saveEvent(eventData);
-    res.json({ message: 'Event created successfully', event: savedEvent })
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Error occured' });
-  }
-  
-});
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT} `)
-  )
+    console.log(`Server is running on http://localhost:${PORT} `)
+  );
 });
 
 // main.handlebars has the default info
